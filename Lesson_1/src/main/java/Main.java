5e3bb3982eac2f1951c15d3d1998b50265279a94
gimbo <gimboy09@mail.ru>
2023-12-14 01:27:15 +0500

    Initial commit

---
+++ Lesson_1/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Writer.printThreeWords();
+        Math.checkSumSign(4,6);
+        ColorPrinter.printColor(5);
+        Math.compareNumbers(1,4);
+    }
+}
