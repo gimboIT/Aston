b430b3aaf4b614d0c18e2f21bd6f504a2a4c420d
gimboIT <137558148+gimboIT@users.noreply.github.com>
2023-12-15 12:30:09 +0500

    Delete Lesson_1 directory

--- Lesson_1/src/main/java/ColorPrinter.java
+++
@@ -1,7 +0,0 @@
-public class ColorPrinter {
-    public static void printColor(int value) {
-        if (value <= 0) System.out.println("Red");
-        else if (value <= 100) System.out.println("Yellow");
-        else System.out.println("Green");
-    }
-}
